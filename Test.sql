e1700191a6e74d0ab21ba915ac190db60a16cf2f
Leonardo Gotardo <125305856+LeoGotardo@users.noreply.github.com>
2023-03-31 09:05:30 -0300

    Create Test.sql

---
+++ Test.sql
@@ -0,0 +1,22 @@
+drop schema if exists NomeDoBanco; -- Deleta o Banco de Dados (Se ja existir um), desnecessario.
+
+create schema NomeDoBanco;  -- Cria o Banco de Dados.
+
+use NomeDoBanco; -- Usa o Banco.
+
+create table NomeDaTabela( -- Cria uma tabela.
+	NomeDoAtributo int  not null  auto_increment,  -- Define um atributo como INT, não permite ser vazio e habilita o Auto-conplete.
+    NomeDoOutroAbributo varchar(50), -- Define um atributo como char(50 espaços).
+    primary key (NomeDoAtributo) -- Define o atributo como chave primaria.
+);
+
+create table OutraTabela(
+	NomeDoAtributo varchar(50), -- 
+    NomeDoOutroAtributo int  not null  auto_increment primary key, -- Define um atributo como INT, não permite ser vazio, habilita o Auto-conplete e defeine como chave primaria.
+	
+    foreign key(NomeDoAtributo) references NomeDaTabela(NomeDoAtributo) -- Define o atributo como uma chave primaria.
+);
+
+insert into NomeDaTabela values(1, "Colombia"); -- Adiciona valores a uma tabela.
+
+select * from NomeDaTabela; -- Mostra a tabela selecionada.
