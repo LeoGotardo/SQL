68d9e83bd626a0d2721157b5dd5bb03d658a9c42
Leonardo Gotardo <125305856+LeoGotardo@users.noreply.github.com>
2023-04-11 08:58:36 -0300

    Update main.sql

--- main.sql
+++ main.sql
@@ -12,18 +12,54 @@ primary key (id_pais));
 create table cidades(
 id_cidade int not null auto_increment primary key,
 nome varchar(50) not null,
-    id_pais int not null,
 UF char(2),
+id_pais int not null,
 foreign key (id_pais) references paises(id_pais));
    
 create table categorias(
 nome varchar(50) not null,
-    id_categoria int not null auto_increment primary key);
+id_categoria int not null auto_increment primary key);
 
-insert into paises values (1, "Brasil");
-insert into paises values (2, "Argentina");
-insert into paises values (3, "Paraguai");
-insert into paises values (4, "Uruguai");
-insert into paises values (5, "Colombia");  
+insert into paises values 
+(1, "Brasil"), 
+(2, "Argentina"),
+(3, "Paraguai"), 
+(4, "Uruguai"), 
+(5, "Colombia");  
 
-SELECT id_pais FROM paises;
+insert into paises(nome) values 
+("Portugal"), 
+("Espanha"),
+("Alemanha"), 
+("Estados Unidos"), 
+("Canadá"),
+("Australia"), 
+("Belgica"),
+("Uganda"), 
+("Nigeria"), 
+("Tokyo"),
+("Porto Rico"), 
+("Piru"),
+("Paquistão"), 
+("Mongolia"), 
+("Mexico");
+
+insert into cidades values
+(1, "Londrina", "PR", 1),
+(2, "Cambé", "PR", 1),
+(3, "São Paulo", "SP", 1),
+(4, "Campinas", "SP", 1),
+(5, "Curitiba", "PR", 1);
+
+insert into cidades(nome, UF, id_pais) values
+("Maringá", "PR", 1),
+("Barracão", "PR", 1),
+("Dionísio Cerqueira", "SC", 1),
+("Buenos Aires", NULL, 2),
+("Curitiba", NULL, 1);
+
+
+SELECT * FROM paises;
+SELECT * FROM cidades;
+
+describe cidades;
